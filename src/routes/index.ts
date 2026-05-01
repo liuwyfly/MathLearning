25d8dfa8de17e42854ef4caaf6e134b39319a5c6
liuweiyin <liuwyok@sina.com>
2026-05-01 19:57:23 +0800

    修复 ping

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,11 @@ import { type FastifyPluginAsync } from 'fastify'
 import { GetContents } from './contentsViews'
 
 const mathLearning: FastifyPluginAsync = async (fastify): Promise<void> => {
+    // ping
+    fastify.get('/ping', async function (request, reply) {
+        return { pong: true, message: "math-learning" }
+    })
+    
     // 获取主目录
     fastify.get('/contents', GetContents)
 }
