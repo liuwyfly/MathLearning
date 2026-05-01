9d8f3f3fdfb0c4e786f56831ea3ec2216cfb3d15
liuweiyin <liuwyok@sina.com>
2026-05-01 09:50:49 +0800

    prisma client 使用默认路径，更方便

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,7 +1,37 @@
+/*
+ * Prisma Client Path
+ * Default:
+ * .\node_modules\@prisma\client
+ * 
+ * Specified:
+ * If the path is specified, it should point to the generated Prisma Client directory 
+ * both in src and dist.
+ * import { PrismaClient } from '../generated/prisma-client'
+ * 
+ * tsconfig.json
+ * "compilerOptions": {
+    "rootDir": "src",
+    "outDir": "dist",
+    "sourceMap": true,
+    "paths": {
+      "@prisma/client": ["./src/generated/prisma-client"]
+    }
+  }
+
+ * schema.prisma
+    generator client {
+    provider = "prisma-client-js"
+    output   = "../src/generated/prisma-client"
+  }
+ *
+ */
+
 import fp from 'fastify-plugin'
-import { PrismaClient } from '../generated/prisma-client'
+import { PrismaClient } from '@prisma/client'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import type { FastifyPluginAsync } from 'fastify'
+
+
 
 // 自定义选项类型
 export interface PrismaPluginOptions {
